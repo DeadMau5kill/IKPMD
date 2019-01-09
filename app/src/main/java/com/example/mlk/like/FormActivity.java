5ad25a3b9496c8f5996a0bc0ba3aba62b56259a2
DeadMau5kill <maurits.kwaak@gmail.com>
2019-01-09 11:00:57 +0100

    Version 0.5

--- app/src/main/java/com/example/mlk/like/FormActivity.java
+++ app/src/main/java/com/example/mlk/like/FormActivity.java
@@ -1,102 +1,101 @@
 package com.example.mlk.like;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.concurrent.TimeUnit;
-import java.util.Calendar;
-
-public class FormActivity extends AppCompatActivity implements View.OnClickListener {
-
-    //Database name + Object creation
-    public static final String database_name = "films_series_data";
-    SQLiteDatabase mDatabase;
-    EditText textName, textComment, textRating;
-    Spinner sort_spinner;
+public class FormActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form);
+        setContentView(R.layout.activity_main);
 
-        //Create the database and insert the table calling "createTable()"
-        mDatabase = openOrCreateDatabase(database_name, MODE_PRIVATE, null);
-        createTable();
 
-        //Create objects for each palette
-        sort_spinner = (Spinner) findViewById(R.id.sort_spinner);
-        textName = (EditText) findViewById(R.id.textName);
-        textComment = (EditText) findViewById(R.id.textComment);
-        textRating = (EditText) findViewById(R.id.textRating);
-        findViewById(R.id.buttonSubmit).setOnClickListener(this);
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    //A class that creates the table within the DB
-    private void createTable() {
-        String createQuery = "CREATE TABLE IF NOT EXISTS filmseries (\n" +
-                "\tid INTEGER NOT NULL CONSTRAINT filmseries_pk PRIMARY KEY AUTOINCREMENT,\n" +
-                "\tname VARCHAR(200),\n" +
-                "\treason VARCHAR(500),\n" +
-                "\trating INT,\n" +
-                "\tsort VARCHAR(15),\n" +
-                "\ttime VARCHAR(30),\n" +
-                "\tCONSTRAINT checkRating CHECK (rating BETWEEN 1 and 10)\n" +
-                "\t);";
-        mDatabase.execSQL(createQuery);
-    }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-    //Add the movie or series to the DB
-    private void addTuple() {
-        //Extract the user input
-        String name = textName.getText().toString().trim();
-        String Comment = textComment.getText().toString().trim();
-        String rating = textRating.getText().toString().trim();
-        String sort = sort_spinner.getSelectedItem().toString();
-        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-
-        //Catch when the user submits empty values
-        if (name.isEmpty()){
-            textName.setError("Geef een naam op");
-            textName.requestFocus();
-            return;
-        }
-        if (Comment.isEmpty()){
-            textComment.setError("Geef commentaar bij de film of serie");
-            textComment.requestFocus();
-            return;
-        }
-        if (rating.isEmpty()){
-            textRating.setError("Geef een rating van 1 t/m 10");
-            textRating.requestFocus();
-            return;
-        }
-
-        //The query to update the DB table
-        String updateQuery = "INSERT INTO filmseries (name, reason, rating, sort, time)\n" +
-                "VALUES (?,?,?,?,?)";
-        mDatabase.execSQL(updateQuery, new String[]{name, Comment, rating, sort, time});
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.buttonSubmit:
-                addTuple();
-                //Notify the user that the operation succeeded
-                Toast.makeText(this, "Je rating is toegevoegd!", Toast.LENGTH_LONG).show();
-
-                //Bring the user to MainActivity when the operation is done
-                Intent mainIntent = new Intent(this, MainActivity.class);
-                startActivity(mainIntent);
-                break;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
+        } else if (id == R.id.nav_film) {
+            Intent filmsIntent = new Intent(this, FilmActivity.class);
+            startActivity(filmsIntent);
+        } else if (id == R.id.nav_series) {
+            Intent seriesIntent = new Intent(this, SeriesActivity.class);
+            startActivity(seriesIntent);
+        } else if (id == R.id.nav_stats) {
+            Intent statsIntent = new Intent(this, StatsActivity.class);
+            startActivity(statsIntent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
