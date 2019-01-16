2e18a8ff5c5051bce239fcb5807bd4a0a348aff2
DeadMau5kill <maurits.kwaak@gmail.com>
2019-01-16 11:03:14 +0100

    Version 0.7.8

--- app/src/main/java/com/example/mlk/like/StatsActivity.java
+++ app/src/main/java/com/example/mlk/like/StatsActivity.java
@@ -51,8 +51,8 @@ public class StatsActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
